fd7214f703c19d3f2143fa3cd6322dcf715d9e3a
Daniel Gehlen <142283217+Daniel-Gehlen@users.noreply.github.com>
2024-04-22 23:58:58 -0300

    Create AuthContext.js

---
+++ AuthContext.js
@@ -0,0 +1,26 @@
+// AuthContext.js
+import React, { createContext, useState } from 'react';
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = (userData) => {
+    localStorage.setItem('user', JSON.stringify(userData));
+    setUser(userData);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('user');
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthProvider, AuthContext };
